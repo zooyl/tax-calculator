9d0f20a29d9454c2be289f89b8ee7469d996d7b9
zooyl <zoiru@protonmail.com>
2022-01-11 14:31:31 +0100

    v1.2 - pdf auto search

--- calculator.py
+++ calculator.py
@@ -9,6 +9,7 @@ from datetime import datetime
 from math import ceil
 
 from local_settings import pdf_file
+# pdf_file = "path/to/file.pdf"
 
 # Settings
 year = 2021
@@ -23,102 +24,59 @@ class CustomHolidayCalendar(AbstractHolidayCalendar):
     for date, name in sorted(country(years=year).items()):
         rules.append(Holiday(name, year=date.year, month=date.month, day=date.day, observance=previous_workday))
  
-
 customBD = CustomBusinessDay(calendar=CustomHolidayCalendar())
-
 
 def main():
     
+    pdf_tax_table = []
+    pdf_div_table = []
     with pdfplumber.open(pdf_file) as pdf:
-        page = pdf.pages[5].find_tables()
-        # t1_content = page[0].extract(x_tolerance = 5)
-        t2_content = page[1].extract()
-        t3_content = page[2].extract()
-        # print(t2_content)
-        # print(t3_content)
-        df_2 = pd.DataFrame(t2_content)
-        df_name = df_2[df_2.columns[0]][0]
-        df_date = df_2[df_2.columns[0]][3:-2]
-        df_withholding_tax = df_2[df_2.columns[2]][3:-2]
-        
-        df_3 = pd.DataFrame(t3_content)
-        # df_name = df_3[df_3.columns[0]][0]
-        # df_date = df_3[df_3.columns[0]][3:-2]
-        df_amount = df_3[df_3.columns[2]][3:-2]
-        
-        previous_b_day = previous_day(df_date)
-        previous_b_day_series = pd.Series(previous_b_day)
-        # cached_rate = cache_nb_rate(previous_b_day_series)
-        cached_rate = [3.9676, 3.8035, 3.9925, 4.1088]
-        
-        pln_tax_paid_usa = tax_calc(df_name, cached_rate, df_withholding_tax)
-        div_pln = div_calc(df_amount, cached_rate)
-        pol_tax = polish_tax_calc(div_pln)
-        diff = diff_to_pay(pol_tax, pln_tax_paid_usa)
-        print(display_info(pln_tax_paid_usa, div_pln, pol_tax, diff, cached_rate))
-        
-            
-def display_info(pln_tax_paid_usa, div_pln, pol_tax, diff, cached_rate):
-    for i, j, k, l, m in zip(pln_tax_paid_usa, div_pln, pol_tax, diff, cached_rate):
-            print("-------")
-            print("NBP RATE: " + str(m))
-            print("DIVIDEND PLN: " + str(j))
-            print("FULL PL TAX: " + str(k))
-            print("PLN TAX PAID IN USA: " + str(i))
-            print("TAX DIFF: " + str(l))
-            print("ROUND UP: " + str(ceil(l)))
+        pages = pdf.pages
+        for i,pg in enumerate(pages):
+            tables = pages[i].extract_tables()
+            for j in tables:
+                if j[0][0] == withholdin_table:
+                    pdf_tax_table.append(j)
+                if j[0][0] == dividends_table:
+                    pdf_div_table.append(j)
     
+    for i, j in zip(pdf_tax_table, pdf_div_table):
+        df_tax = pd.DataFrame(i)
+        df_name = df_tax[df_tax.columns[0]][0]
+        df_date = df_tax[df_tax.columns[0]][3:-2]
+        df_withholding_tax = df_tax[df_tax.columns[2]][3:-2]
+        df_div = pd.DataFrame(j)
+        df_div_amount = df_div[df_div.columns[2]][3:-2]
         
-def cache_nb_rate(previous_b_day_series):
-    nb_rate_list = []
-    for i in previous_b_day_series:
-            nb_rate_list.append(get_nb_mid_rate(currency, i.date()))
-    return nb_rate_list
-          
-          
+    previous_b_day = previous_day(df_date)
+    previous_b_day_series = pd.Series(previous_b_day)
+    
+    cached_rate = cache_nb_rate(previous_b_day_series)
+    # cached_rate = [3.9676, 3.8035, 3.9925, 4.1088]
+    
+    pln_tax_paid_usa = tax_calc(df_name, cached_rate, df_withholding_tax)
+    div_pln = div_calc(df_div_amount, cached_rate)
+    pol_tax = polish_tax_calc(div_pln)
+    diff = diff_to_pay(pol_tax, pln_tax_paid_usa)
+    print(display_info(pln_tax_paid_usa, div_pln, pol_tax, diff, cached_rate))
+        
+        
 def previous_day(df_date):
     previous_day = []
     for i in df_date:
         datetime_object = convert_date_str_to_timeobject(i)
         previous_day.append(datetime_object - customBD)
     return previous_day
-
-
-def tax_calc(df_name, cached_rate, df_withholding_tax):
-    pln_tax_paid_usa = []
-    if (df_name == withholdin_table):
-        for i, j in zip(df_withholding_tax, cached_rate):
-            pln_tax_paid_usa.append(round(abs(float(i)) * j, 2))
-        return pln_tax_paid_usa
-        
-        
-def div_calc(df_amount, cached_rate):
-    div_in_pln = []
-    for i, j in zip(df_amount, cached_rate):
-        div_in_pln.append(round(float(i) * j, 2))
-    return div_in_pln
-
-        
-def polish_tax_calc(div_pln):
-    pol = []
-    for i in div_pln:
-        pol.append(round(i * polish_tax, 2))
-    return pol
-        
-        
-def diff_to_pay(pol_tax, pln_tax_paid_usa):
-    diff = []
-    if len(pol_tax) == len(pln_tax_paid_usa):
-        for tax_pl, tax_paid in zip(pol_tax, pln_tax_paid_usa):
-            difference = tax_pl - tax_paid
-            diff.append(round(difference, 2))
-        return diff
-
-
+  
 def convert_date_str_to_timeobject(str_date):
     obj_date = datetime.strptime(str_date, "%Y-%m-%d")
     return obj_date
-
+      
+def cache_nb_rate(previous_b_day_series):
+    nb_rate_list = []
+    for i in previous_b_day_series:
+            nb_rate_list.append(get_nb_mid_rate(currency, i.date()))
+    return nb_rate_list
 
 def get_nb_mid_rate(currency, date):
     try:
@@ -132,11 +90,47 @@ def get_nb_mid_rate(currency, date):
             return mid
     except requests.exceptions.HTTPError as err:
         raise SystemExit(err)
-    
 
+def tax_calc(df_name, cached_rate, df_withholding_tax):
+    pln_tax_paid_usa = []
+    if (df_name == withholdin_table):
+        for i, j in zip(df_withholding_tax, cached_rate):
+            pln_tax_paid_usa.append(round(abs(float(i)) * j, 2))
+        return pln_tax_paid_usa
+           
+def div_calc(df_div_amount, cached_rate):
+    div_in_pln = []
+    for i, j in zip(df_div_amount, cached_rate):
+        div_in_pln.append(round(float(i) * j, 2))
+    return div_in_pln
+   
+def polish_tax_calc(div_pln):
+    pol = []
+    for i in div_pln:
+        pol.append(round(i * polish_tax, 2))
+    return pol
+             
+def diff_to_pay(pol_tax, pln_tax_paid_usa):
+    diff = []
+    if len(pol_tax) == len(pln_tax_paid_usa):
+        for tax_pl, tax_paid in zip(pol_tax, pln_tax_paid_usa):
+            difference = tax_pl - tax_paid
+            diff.append(round(difference, 2))
+        return diff
+    
+def display_info(pln_tax_paid_usa, div_pln, pol_tax, diff, cached_rate):
+    for i, j, k, l, m in zip(pln_tax_paid_usa, div_pln, pol_tax, diff, cached_rate):
+            print("-------")
+            print("NBP RATE: " + str(m))
+            print("DIVIDEND PLN: " + str(j))
+            print("FULL PL TAX: " + str(k))
+            print("PLN TAX PAID IN USA: " + str(i))
+            print("TAX DIFF: " + str(l))
+            print("ROUND UP: " + str(ceil(l)))
+    
+    
 if __name__ == "__main__":
     main()
-
 
 # Sources
 
